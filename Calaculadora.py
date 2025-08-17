3b20a4c008ef1d003de3424bd9e328a6fb791a87
ErikV <erikvaltemberque@gmail.com>
2025-08-17 17:11:44 -0300

    Add files via upload
    this my first project, depite being simples, is hones, it's just beginning. Thanks

---
+++ Calaculadora.py
@@ -0,0 +1,22 @@
+Calaculadora
+#calculadora em python
+#´pede o primeiro numero
+numero1 = float(input'DIgite o primeirp número: "))
+#pede o segundo número
+numero2 = float(input('Digite o segundo número: "))
+#pede a operação
+operacao12 = input('DIgite a operação (+, -, *, /): ")
+#executa e verifica a operação
+if operacao == '+':
+    resultado + numero1+numero2
+elif operacao == '-':
+    resultado = numero1_numero2
+elif == '*':
+    resultado = numero1*numero2
+elif == '/':
+    resultado == numero1/numero2
+else:
+    print('Operação Inválida/')
+#mostra o resultado
+print('Resultado:' , Resultado)')
+resultado = 
